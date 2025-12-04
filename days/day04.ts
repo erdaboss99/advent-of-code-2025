d807f6eaf58837b3754fb765086fab941b70e719
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2025-12-04 12:46:23 +0100

    optimize solutions for day four regarding runtime

--- days/day04.ts
+++ days/day04.ts
@@ -19,16 +19,19 @@ type Position = { x: number; y: number };
 
 type Location = Position & { sign: Sign };
 
-type Grid = Location[];
+type LocationKey = `${number}-${number}`;
+
+type Grid = Map<LocationKey, Location>;
 
 const parseInput = (input: string): Grid => {
 	const lines = input.split('\n');
-	return lines.flatMap((line, x) =>
-		line.split('').map((char, y) => ({
-			x,
-			y,
-			sign: char as Sign,
-		})),
+	return new Map<LocationKey, Location>(
+		lines.flatMap((line, y) =>
+			[...line].map((char, x) => {
+				const loc: Location = { x, y, sign: char as Sign };
+				return [`${x}-${y}` as LocationKey, loc];
+			}),
+		),
 	);
 };
 
@@ -49,22 +52,24 @@ const isAccessible = (location: Location, grid: Grid): boolean => {
 	const adjacentLocations = DIRECTIONS.map(([dx, dy]) => {
 		const nx = location.x + dx;
 		const ny = location.y + dy;
-		return grid.find((loc) => loc.x === nx && loc.y === ny);
+		return grid.get(`${nx}-${ny}`);
 	}).filter((loc) => loc !== undefined);
 
 	return adjacentLocations.filter((loc) => loc.sign === '@').length < 4;
 };
 
 const sumPossibleMoves = (grid: Grid): number => {
-	return grid.reduce((acc, loc) => {
-		if (isAccessible(loc, grid)) acc++;
-		return acc;
-	}, 0);
+	let count = 0;
+	for (const loc of grid.values()) {
+		if (isAccessible(loc, grid)) count++;
+	}
+	return count;
 };
 
 function part1(_input: string): number {
 	// const grid = parseInput(_dummyInput);
 	const grid = parseInput(_input);
+
 	return sumPossibleMoves(grid);
 }
 
@@ -72,10 +77,14 @@ const simulateExecution = (currentGrid: Grid, totalMoves = 0): number => {
 	const moveCount = sumPossibleMoves(currentGrid);
 	if (moveCount === 0) return totalMoves;
 
-	const newGrid = currentGrid.map<Location>((loc) => {
-		if (isAccessible(loc, currentGrid)) return { ...loc, sign: '.' };
-		return loc;
-	});
+	const newGrid = new Map<LocationKey, Location>();
+	for (const [key, loc] of currentGrid) {
+		if (isAccessible(loc, currentGrid)) {
+			newGrid.set(key, { ...loc, sign: '.' });
+		} else {
+			newGrid.set(key, loc);
+		}
+	}
 
 	return simulateExecution(newGrid, totalMoves + moveCount);
 };
